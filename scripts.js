f817742cabcd9aeae4881e41e2a45109a24c5f01
Justyna G <justykag@wp.pl>
2017-08-03 11:38:15 +0200

    poprawa

--- scripts.js
+++ scripts.js
@@ -2,16 +2,19 @@ function Phone(brand, price, color) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
-}
+};
+
 Phone.prototype.printInfo = function() {
-	console.log("Phone brand: " + this.brand + ", color: " + this.color + ", price: " + this.price);
+	var getWarrantyCost = this.price*0.1;
 
-}
+	console.log("Phone brand: " + this.brand + ", color: " + this.color + ", price: " + this.price + ", warranty cost: " + getWarrantyCost);
+
+};
 
 
-var iPhone6 = new Phone("Apple", "1700 PLN", "złoty" );
-var SamsungGalaxyS6 = new Phone("Samsung", "1800 PLN", "biały" );
-var OnePlusOne = new Phone("OnePlus", "1999 PLN", "czarny");
+var iPhone6 = new Phone("Apple", 1700 , "złoty" );
+var SamsungGalaxyS6 = new Phone("Samsung", 1800 , "biały" );
+var OnePlusOne = new Phone("OnePlus", 1999 , "czarny");
 
 iPhone6.printInfo()
 SamsungGalaxyS6.printInfo()
